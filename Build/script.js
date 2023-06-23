df22144bae51afa05d3a450e226551e4e714e9f2
Janka Antalova <124877345+Sleepyhead95@users.noreply.github.com>
2023-06-23 19:49:44 +0200

    add pop-up window to showcase certificate

--- Build/script.js
+++ Build/script.js
@@ -1,15 +1,15 @@
 // Function to add animation to Hello
-const textElement = document.getElementById('hello');
-const text = textElement.textContent;
-let animatedText = '';
-const animationDuration = 1; // Total animation duration in seconds
+//const textElement = document.getElementById('hello');
+//onst text = textElement.textContent;
+//let animatedText = '';
+//const animationDuration = 1; // Total animation duration in seconds
 
-for (let i = 0; i < text.length; i++) {
-  const delay = (animationDuration / text.length) * i;
-  animatedText += `<span class="animated-letter" style="animation-delay: ${delay}s">${text[i]}</span>`;
-}
+//for (let i = 0; i < text.length; i++) {
+ // const delay = (animationDuration / text.length) * i;
+  //animatedText += `<span class="animated-letter" style="animation-delay: ${delay}s">${text[i]}</span>`;
+//}
 
-textElement.innerHTML = animatedText;
+//textElement.innerHTML = animatedText;
 
 // Function to toggle project details
 function toggleProjectDetails(event) {
@@ -22,11 +22,33 @@ function toggleProjectDetails(event) {
 
 // Function to blur header on scroll
 window.addEventListener('scroll', function() {
-  var header = document.getElementById('header');
+  const header = document.getElementById('header');
   if(window.scrollY > header.offsetHeight) {
     header.classList.add('blur');
   } else {
     header.classList.remove('blur');
   }
 });
+
+// Function for pop-up window
+document.addEventListener('DOMContentLoaded', (event) => {
+  const modal = document.getElementById("myModal");
+  const btn = document.getElementById("my-button");
+  const span = document.getElementById("close");
+
+  btn.addEventListener('click', function() {
+      modal.style.display = "block";
+  })
+
+  span.addEventListener('click', function() {
+      modal.style.display = "none";
+  })
+
+  window.addEventListener('click', function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  })
+});
+
 
